ca59d83fa3e6482238e749eeb1516dcff991859b
อาทิตย์ ขาวเอี่ยม <atit@sMacBookPro.local>
2024-07-03 18:56:43 +0700

    up

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environments = {
   production: false,
-  baseURLAPI: 'http://ec2-3-1-218-203.ap-southeast-1.compute.amazonaws.com:8081/api'
+  baseURLAPI: 'https://quotation.atit.site/api'
 };
